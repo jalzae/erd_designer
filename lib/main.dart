8874113eb22f08c8d3583e1c6cad8982026c9af6
JalzAe <jalzae@gmail.com>
2023-11-05 02:06:35 +0700

    single table

--- lib/main.dart
+++ lib/main.dart
@@ -57,7 +57,7 @@ class ERDPage extends StatelessWidget {
   }
 }
 
-class TableWidget extends StatelessWidget {
+class TableWidget extends StatefulWidget {
   final String tableName;
   final List<ColumnWidget> columns;
 
@@ -67,28 +67,103 @@ class TableWidget extends StatelessWidget {
   });
 
   @override
+  _TableWidgetState createState() => _TableWidgetState();
+}
+
+class _TableWidgetState extends State<TableWidget> {
+  Offset position = Offset(0, 0);
+  double maxWidth=0;
+  double maxHeight=0;
+
+  @override
+  void initState() {
+    super.initState();
+    maxWidth = 150.0; // Set your desired max width
+    maxHeight = 300; // Initial max height
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      constraints: const BoxConstraints(maxWidth: 150),
-      decoration: BoxDecoration(
-        border: Border.all(color: Colors.blue, width: 2.0),
-        borderRadius: BorderRadius.circular(5.0),
-      ),
-      child: IntrinsicHeight(
-        child: Column(
-          children: [
-            Text(
-              tableName,
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 18,
+    return Stack(
+      children: [
+        Positioned(
+          left: position.dx,
+          top: position.dy,
+          child: Draggable(
+            feedback: Card(
+              // Wrap the content with a Card
+              child: Container(
+                width: maxWidth,
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.blue, width: 2.0),
+                  borderRadius: BorderRadius.circular(5.0),
+                ),
+                child: IntrinsicHeight(
+                  child: Column(
+                    children: [
+                      Text(
+                        widget.tableName,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                      ),
+                      Divider(color: Colors.blue),
+                      // Use a Column with SingleChildScrollView
+                      SingleChildScrollView(
+                        child: Column(
+                          children: widget.columns,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ),
             ),
-            Divider(color: Colors.blue),
-            ...columns,
-          ],
+            childWhenDragging: Container(),
+            child: Card(
+              // Wrap the content with a Card
+              child: Container(
+                width: maxWidth,
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.blue, width: 2.0),
+                  borderRadius: BorderRadius.circular(5.0),
+                ),
+                child: IntrinsicHeight(
+                  child: Column(
+                    children: [
+                      Text(
+                        widget.tableName,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                      ),
+                      Divider(color: Colors.blue),
+                      // Use a Column with SingleChildScrollView
+                      SingleChildScrollView(
+                        child: Column(
+                          children: widget.columns,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            onDraggableCanceled: (Velocity velocity, Offset offset) {
+              setState(() {
+                position = Offset(
+                  offset.dx
+                      .clamp(0.0, MediaQuery.of(context).size.width - maxWidth),
+                  offset.dy.clamp(
+                      0.0, MediaQuery.of(context).size.height - maxHeight),
+                );
+              });
+            },
+          ),
         ),
-      ),
+      ],
     );
   }
 }
